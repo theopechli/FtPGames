fd8011455aebd703757959e6379e2587c1f10adb
Theofilos Pechlivanis <theofilos.pechlivanis@gmail.com>
2022-11-26 15:01:58 +0200

    Add room dep and define game entity

--- app/src/main/java/com/theopechli/ftpgames/model/Game.kt
+++ app/src/main/java/com/theopechli/ftpgames/model/Game.kt
@@ -1,19 +1,32 @@
 package com.theopechli.ftpgames.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+@Entity
 @Serializable
 data class Game(
+    @PrimaryKey
     val id: Int,
     val title: String,
     val thumbnail: String,
-    @SerialName("short_description") val shortDescription: String,
-    @SerialName("game_url") val gameUrl: String,
+    @ColumnInfo(name = "short_description")
+    @SerialName("short_description")
+    val shortDescription: String,
+    @ColumnInfo (name = "game_url")
+    @SerialName("game_url")
+    val gameUrl: String,
     val genre: String,
     val platform: String,
     val publisher: String,
     val developer: String,
-    @SerialName("release_date") val releaseDate: String,
-    @SerialName("freetogame_profile_url") val profileUrl: String
+    @ColumnInfo(name = "release_date")
+    @SerialName("release_date")
+    val releaseDate: String,
+    @ColumnInfo(name = "freetogame_profile_url")
+    @SerialName("freetogame_profile_url")
+    val profileUrl: String
 )
