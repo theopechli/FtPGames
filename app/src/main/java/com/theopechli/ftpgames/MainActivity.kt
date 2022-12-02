919504eddee209c5207f2d743b1c2c6826a94415
Theofilos Pechlivanis <theofilos.pechlivanis@gmail.com>
2022-12-02 16:30:37 +0200

    Update System UI status bar based on theme

--- app/src/main/java/com/theopechli/ftpgames/MainActivity.kt
+++ app/src/main/java/com/theopechli/ftpgames/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.theopechli.ftpgames
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.graphics.toArgb
 import com.theopechli.ftpgames.ui.FtPGamesApp
 import com.theopechli.ftpgames.ui.theme.FtPGamesTheme
@@ -13,7 +17,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FtPGamesTheme {
-                this.window.statusBarColor = Color.Black.toArgb()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    window.insetsController?.apply {
+                        setSystemBarsAppearance(
+                            if (isSystemInDarkTheme()) 0 else APPEARANCE_LIGHT_STATUS_BARS,
+                            APPEARANCE_LIGHT_STATUS_BARS
+                        )
+                    }
+                } else {
+                    @Suppress("DEPRECATION")
+                    window.decorView.systemUiVisibility =
+                        (if (isSystemInDarkTheme()) 0 else View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+                }
+                this.window.statusBarColor = MaterialTheme.colors.primary.toArgb()
+                this.window.navigationBarColor = MaterialTheme.colors.primary.toArgb()
                 FtPGamesApp()
             }
         }
